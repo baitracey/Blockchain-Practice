6325bf5e323172fb0a7072bf7c7ea81acf54274b
Bailey Tracey <baileytracey2003@gmail.com>
2025-04-29 22:20:33 +0100

    created blockchain class

--- Blockchain practice/Blockchain_practice.py
+++ Blockchain practice/Blockchain_practice.py
@@ -1,1 +1,25 @@
+# Blockchain
+# Blockchain is a decentralized, distributed ledger technology that allows multiple parties to maintain a shared database without the need for a central authority. It is the underlying technology behind cryptocurrencies like Bitcoin and Ethereum.
 
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        pass
+    
+    def new_transaction(self):
+        # Adds a new transaction to the list of transactions
+        pass
+    
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        pass
